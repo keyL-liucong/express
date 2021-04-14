e069e9ab6f5c61a2f486877ae0ebb9be24a21ff8
caodingshuan <824019782@qq.com>
2021-04-15 03:11:12 +0800

    request

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,3 +1,31 @@
-export const request = () => {
-    
+const BASE_URL = '';
+export const request = (url, data, method = 'GET', loading = true) => {
+    return new Promise((resolve, reject) => {
+        if (loading) {
+            uni.showLoading({
+                title: '加载中'
+            });
+        }
+        uni.request({
+            url: BASE_URL + url, //仅为示例，并非真实接口地址。
+            data,
+            method, // 默认值 GET
+            header: {
+                'token': 'hello W' //自定义请求头信息
+            },
+            success: (res) => {
+                resolve(res);
+                console.log('请求成功', res);
+            },
+            fail: (err) => {
+                reject(err);
+                console.log('请求失败', err);
+            },
+            complete: () => {
+                if (loading) {
+                    uni.hideLoading();
+                }
+            }
+        });
+    })
 }
