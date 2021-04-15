63001a5d70fcb5e4ffad1cce6c8ba7bd0fda0229
LYC <824019782@qq.com>
2021-04-16 04:06:40 +0800

    tabbar

---
+++ src/pages/tabbar-5/index.vue
@@ -0,0 +1,29 @@
+<template>
+<div class='app-container'>
+    qqqqqq
+</div>
+</template>
+
+<script>
+
+export default {
+components: {},
+data() {
+return {
+
+};
+},
+methods: {
+
+},
+created() {
+
+},
+mounted() {
+
+},
+}
+</script>
+<style lang='scss' scoped>
+
+</style>
