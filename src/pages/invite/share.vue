d6b7bc74b33e8a65b59bed96fd1ae6db2a75149a
zhitao.mu <muzhitao@zhuge.com>
2021-05-12 17:43:49 +0800

    update

--- src/pages/invite/share.vue
+++ src/pages/invite/share.vue
@@ -1,7 +1,9 @@
 <template>
     <view class="app-container">
 		<view class="app-body">
-			<view class="body-step_1"></view>
+			<view class="body-step_1">
+				
+			</view>
 			<view class="body-step_2"></view>
 			<view class="body-step_3"></view>
 			<view class="body-step_4"></view>
@@ -32,5 +34,12 @@
 		position:fixed; 
 		background-size:100% 100%;
 		z-index: -1;
+	}
+	.app-body{
+		padding: 0rpx 20rpx;
+		.body-step_1,.body-step_2,.body-step_3,.body-step_4,.body-step_5{
+			background-color: #FFEED9;
+			border-radius: 16rpx;
+		}
 	}
 </style>
