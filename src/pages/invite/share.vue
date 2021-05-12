b2e9dff27bd34c03df55beb81f237de800acb8c3
zhitao.mu <muzhitao@zhuge.com>
2021-05-12 17:22:04 +0800

    我的邀请

--- src/pages/invite/share.vue
+++ src/pages/invite/share.vue
@@ -1,8 +1,36 @@
 <template>
+    <view class="app-container">
+		<view class="app-body">
+			<view class="body-step_1"></view>
+			<view class="body-step_2"></view>
+			<view class="body-step_3"></view>
+			<view class="body-step_4"></view>
+			<view class="body-step_5"></view>
+		</view>
+	</view>
 </template>
 
 <script>
+	export default {
+		components: {},
+		data() {
+			return {
+			};
+		},
+		methods: {
+		}
+	};
 </script>
 
-<style>
+<style lang='scss' scoped>
+	page{
+		height:100%;
+	}
+	.background {
+		width: 100%;
+		height: 100%;
+		position:fixed; 
+		background-size:100% 100%;
+		z-index: -1;
+	}
 </style>
