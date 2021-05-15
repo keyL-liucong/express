0de75634acbcda5d44d6795c5af66e049547bdf0
muzhitao <695753037@qq.com>
2021-05-15 15:55:33 +0800

    邀请地址

--- src/pages/invite/address.vue
+++ src/pages/invite/address.vue
@@ -14,7 +14,7 @@
 							<text class="get">收</text>
 							<text class="get_address">收件地址</text>
 						</view>
-						<view class="tui-center tab-active-no tui-col-4">
+						<view class="tui-right tab-active-no tui-col-4">
 							<text class="clear">清空</text>
 						</view>
 					</view>
@@ -40,9 +40,9 @@
 								<text class="en">County/Reguon</text>
 							</view>
 							<view class="tui-left tui-col-8 tui-middle">
-								<picker @change="bindPickerChange" :value="indexEn" :range="countryEn" range-key="name">
-									<view class="uni-input">{{ currentCountryName }}</view>
-								</picker>
+								<view class="tui-left tui-col-8 tui-middle">
+									<text class="input-data" :class="sureData.currentCountry != '请选择地区' ? 'cur': ''" @click="popupClick(2, 0)">{{ sureData.currentCountry }}</text>
+								</view>
 							</view>
 						</view>
 					</tui-list-cell>
@@ -59,7 +59,6 @@
 									</view>
 									<view class="tui-center tui-col-10">
 										<input placeholder="电话号码" v-model="postEnData.mobile" />
-										
 									</view>
 								</view>
 							</view>
@@ -83,10 +82,22 @@
 								<text class="en">State</text>
 							</view>
 							<view class="tui-left tui-col-8 tui-middle">
-								<picker @change="bindPickerStateChange" :value="state.stateIndex" :range="state.stateData" range-key="name">
-									<view class="uni-input">{{ state.stateName }}</view>
-								</picker>
-								<!-- <input placeholder="请选择" /> -->
+								<view class="tui-left tui-col-8 tui-middle">
+									<text class="input-data" :class="sureData.currentProv != '请选择' ? 'cur': ''" @click="popupClick(2, 1)">{{ sureData.currentProv }}</text>
+								</view>
+							</view>
+						</view>
+					</tui-list-cell>
+					<tui-list-cell :arrow="false" :lineRight="true" :hover="false" padding="15rpx" color="#000000">
+						<view class="tui-flex tui-align-between">
+							<view class="tui-left tui-col-4 row">
+								<text class="zh">城市</text>
+								<text class="en">City</text>
+							</view>
+							<view class="tui-left tui-col-8 tui-middle">
+								<view class="tui-left tui-col-8 tui-middle">
+									<text class="input-data" :class="sureData.currentCity != '请选择' ? 'cur': ''" @click="popupClick(2, 2)">{{ sureData.currentCity }}</text>
+								</view>
 							</view>
 						</view>
 					</tui-list-cell>
@@ -121,7 +132,7 @@
 								<text class="zh">姓名</text>
 							</view>
 							<view class="tui-left tui-col-8 tui-middle">
-								<input placeholder="请输入名字" />
+								<input placeholder="请输入名字" v-model="postEnData.realname" />
 							</view>
 						</view>
 					</tui-list-cell>
@@ -131,7 +142,7 @@
 								<text class="zh">地区</text>
 							</view>
 							<view class="tui-left tui-col-8 tui-middle">
-								<input placeholder="请选择地区" />
+								<text class="input-data" :class="sureData.currentCountry != '请选择地区' ? 'cur': ''" @click="popupClick(1, 0)">{{ sureData.currentCountry }}</text>
 							</view>
 						</view>
 					</tui-list-cell>
@@ -141,7 +152,14 @@
 								<text class="zh">电话</text>
 							</view>
 							<view class="tui-left tui-col-8 tui-middle">
-								<input placeholder="请输入电话号码" />
+								<view class="tui-flex">
+									<view class="tui-center tui-col-2 mobile_code">
+										<input placeholder="区号" v-model="postEnData.mobile_code" />
+									</view>
+									<view class="tui-center tui-col-10">
+										<input placeholder="电话号码" v-model="postEnData.mobile" />
+									</view>
+								</view>
 							</view>
 						</view>
 					</tui-list-cell>
@@ -151,7 +169,7 @@
 								<text class="zh">邮编</text>
 							</view>
 							<view class="tui-left tui-col-8 tui-middle">
-								<input placeholder="请输入邮编" />
+								<input placeholder="请输入邮编" v-model="postEnData.zipcode" />
 							</view>
 						</view>
 					</tui-list-cell>
@@ -161,7 +179,21 @@
 								<text class="zh">城市</text>
 							</view>
 							<view class="tui-left tui-col-8 tui-middle">
-								<input placeholder="请选择" />
+								<view class="tui-left tui-col-8 tui-middle">
+									<text class="input-data" :class="sureData.currentProv != '请选择' ? 'cur': ''" @click="popupClick(1, 1)">{{ sureData.currentProv }}</text>
+								</view>
+							</view>
+						</view>
+					</tui-list-cell>
+					<tui-list-cell :arrow="false" :lineRight="true" :hover="false" padding="30rpx" color="#000000">
+						<view class="tui-flex tui-align-between">
+							<view class="tui-left tui-col-4 row">
+								<text class="zh">区</text>
+							</view>
+							<view class="tui-left tui-col-8 tui-middle">
+								<view class="tui-left tui-col-8 tui-middle">
+									<text class="input-data" :class="sureData.currentCity != '请选择' ? 'cur': ''" @click="popupClick(1, 2)">{{ sureData.currentCity }}</text>
+								</view>
 							</view>
 						</view>
 					</tui-list-cell>
@@ -171,7 +203,7 @@
 								<text class="zh">详细地址</text>
 							</view>
 							<view class="tui-left tui-col-8 tui-middle">
-								<input placeholder="精确到门牌号" />
+								<input placeholder="精确到门牌号" v-model="postEnData.address" />
 							</view>
 						</view>
 					</tui-list-cell>
@@ -181,7 +213,7 @@
 								<text class="zh">公司名称</text>
 							</view>
 							<view class="tui-left tui-col-8 tui-middle">
-								<input placeholder="(选填)" />
+								<input placeholder="(选填)" v-model="postEnData.company_name" />
 							</view>
 						</view>
 					</tui-list-cell>
@@ -193,6 +225,33 @@
 				<tui-button margin="0 20rpx 26rpx 0" type="green" width="368rpx" height="90rpx" :size="32" @click="submit">确定</tui-button>
 			</view>
 		</view>
+		<uni-popup ref="showpopup" type="bottom">
+			<view class="pupup-block">
+				<view class="countryZh-block">
+					<view class="header">
+						<text>{{ requstCountry.popupTitle }}</text>
+					</view>
+					<view class="body">
+						<scroll-view scroll-y="true" class="scroll-Y">
+							<tui-list-view color="#777" unlined="all">
+								<tui-list-cell v-for="(item,index) in requstCountry.countryData" :key="index" :arrow="false" :lineRight="true" :hover="false" padding="20rpx" color="#000000">
+									<view class="tui-flex tui-align-between" @click="checkData(item, requstCountry.currentTab)">
+										<view class="tui-left tui-col-12" >
+											<text class="country-name" :class="requstCountry.currentId == item.id ? 'cur':''">{{ item.name }}</text>
+											<text class="country-name-en" :class="requstCountry.currentId == item.id ? 'cur':''">{{ item.name_en }}</text>
+										</view>
+										
+									</view>
+								</tui-list-cell>
+							</tui-list-view>
+						</scroll-view>
+					</view>
+					<view class="tui-center">
+						<tui-button width="686rpx" height="80rpx" shape="circle" type="warning" @click="sure()">确定</tui-button>
+					</view>
+				</view>
+			</view>
+		</uni-popup>
 	</view>
 </template>
 
@@ -222,10 +281,96 @@
 					zipcode:"",
 					address:"",
 					company_name:"",
-				}
+				},
+				requstCountry:{
+					is_china:1,
+					countryData:[],
+					currentCountry:"请选择地区",
+					currentProv:"请选择",
+					currentProvId:"",
+					currentCity:"请选择",
+					country:"",
+					country_id:"",
+					currentTab:0,
+					popupTitle:"",
+					currentId:""
+				},
+				
+				sureData:{
+					is_china:1,
+					countryData:[],
+					currentCountry:"请选择地区",
+					currentProv:"请选择",
+					currentProvId:"",
+					currentCity:"请选择",
+					country:"",
+					country_id:"",
+					currentTab:0,
+					popupTitle:"",
+					currentId:""
+				},
+				
+				popupTitle:["请选择收件国","请选择州","请选择城市"]
 			};
 		},
 		methods: {
+			sure() {
+				let requstCountry = this.requstCountry
+				this.sureData = {
+					is_china: requstCountry.is_china,
+					countryData:requstCountry.countryData,
+					currentCountry:requstCountry.currentCountry,
+					currentProv:requstCountry.currentProv,
+					currentProvId:requstCountry.currentProvId,
+					currentCity:requstCountry.currentCity,
+					country:requstCountry.country,
+					country_id:requstCountry.country_id,
+					currentTab:requstCountry.currentTab,
+					popupTitle:requstCountry.popupTitle,
+					currentId:requstCountry.currentId
+				}
+				this.postEnData.country_id = this.sureData.country_id;
+				this.postEnData.state_id = this.sureData.currentProvId;
+				this.postEnData.city_id = this.sureData.currentCity;
+				this.$refs['showpopup'].close()
+			},
+			checkData(e, currentTab) {
+				
+				if(currentTab == 0) {
+					this.requstCountry.country_id = e.id;
+					this.requstCountry.currentCountry = e.name;
+				} else if(currentTab == 1) {
+					
+					this.requstCountry.currentProv = e.name;
+					this.requstCountry.currentProvId = e.id;
+				} else if(currentTab == 2) {
+					
+					this.requstCountry.currentCity = e.name;
+				}
+				this.requstCountry.currentId = e.id;
+				
+			},
+			//currentTab-0:省 ，1：州，2-城市
+			async popupClick(is_china, currentTab) {
+				
+				this.requstCountry.currentTab = currentTab;
+				this.requstCountry.popupTitle = this.popupTitle[currentTab];
+				if(currentTab == 0) {
+					let countryData = await this.$api.getCountryAddrList({is_china:is_china})
+					this.requstCountry.countryData = countryData.data;
+				} else if(currentTab == 1) {
+					let countryData = await this.$api.getRegionEnAddr({country_id:this.requstCountry.country_id});
+					this.requstCountry.countryData = countryData.data;
+				} else if(currentTab == 2) {
+					let countryData = await this.$api.getRegionEnAddr({country_id:this.requstCountry.country_id,parent_id:this.requstCountry.currentProvId});
+					this.requstCountry.countryData = countryData.data;
+				}
+				this.requstCountry.is_china = is_china;
+				
+				this.$nextTick(() => {
+					this.$refs['showpopup'].open()
+				})
+			},
 			async submit() {
 				if(this.currentTab == 1) {
 					let data = this.postEnData;
@@ -273,46 +418,27 @@
 					return;
 				}
 				this.currentTab = e;
-			},
-			bindPickerStateChange(e) {
-				
-				if(!this.postEnData.country_id) {
-					this.$toast("请选择国家");
-					return;
-				}
-				let state = this.state.stateData;
-				this.state.stateName = state[e.detail.value].name
-				this.postEnData.state_id = state[e.detail.value].id
-				
-			},
-			bindPickerChange: function(e) {
-				let country = this.countryEn;
-				this.currentCountryName = country[e.detail.value].name;
-				this.postEnData.country_id = country[e.detail.value].id;
-				this.indexEn = e.detail.value
-				this.getState(this.postEnData.country_id);
-			},
-			async getState(country_id) {
-				let res = await this.$api.getRegionEnAddr({country_id:country_id})
-				console.log(res);
-				if(res.data) {
-					this.state.stateData = res.data;
-				}
-				
-			},
-			async getCountry() {
-				let EnRes = await this.$api.getCountryAddrList({is_china:2})
-				if(EnRes.data) {
-					this.countryEn = EnRes.data;
-				}
-				let ZhRes = await this.$api.getCountryAddrList({is_china:1})
-				if(ZhRes.data) {
-					this.countryZh = ZhRes.data;
-				}
+				this.requstCountry = this.sureData = {
+					is_china:1,
+					countryData:[],
+					currentCountry:"请选择地区",
+					currentProv:"请选择",
+					currentProvId:"",
+					currentCity:"请选择",
+					country:"",
+					country_id:"",
+					currentTab:0,
+					popupTitle:"",
+					currentId:""
+				},
+				this.postEnData.country_id = "";
+				this.postEnData.state_id = "";
+				this.postEnData.city_id = "";
+				console.log(this.postEnData);
 			}
 		},
 		async created() {
-			this.getCountry()
+			
 		},
 		mounted() {},
 	};
@@ -327,7 +453,9 @@
 			background-color: #FFFFFF;
 			border-radius: 16rpx;
 			overflow: hidden;
-
+			.cur{
+				color: #000000 !important; 
+			}
 			.app-body-tab {
 				height: 96rpx;
 				line-height: 104rpx;
@@ -387,12 +515,50 @@
 			margin-top: 60rpx;
 		}
 	}
-
+	.pupup-block{
+		padding: 20rpx;
+		background-color: #FFFFFF;
+		.cur{
+			color: #FF6D00 !important;
+		}
+		.countryZh-block{
+			.header{
+				
+				text{
+					color: #7B7B7B;
+				}
+			}
+			.country-name{
+				font-size: 26rpx;
+				margin-right: 8rpx;
+			}
+			.country-name-en{
+				font-size: 26rpx;
+			}
+			.body{
+				margin: 20rpx 0rpx;
+			}
+		}
+		
+	}
+	.tui-right{
+		text-align: right;
+	}
 	.row {
 		display: flex;
 		flex-direction: column;
 	}
 	.mobile_code{
 		border-right: 2rpx #7B7B7B solid;
+	}
+	.input-data{
+		color: #7B7B7B;
+		font-size: 28rpx;
+	}
+	.scroll-Y{
+		height: 300rpx;
+	}
+	.activie{
+		color: #FF6D00;
 	}
 </style>
