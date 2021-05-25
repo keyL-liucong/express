490cefaa6a29d3d442923fa460f5756c0b5ada75
lsn5211314 <244078074@qq.com>
2021-05-25 16:41:04 +0800

    add

---
+++ src/pages/login/sign_text.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="app-container">
+        <rich-text type="text" :nodes="signText"></rich-text>
+    </div>
+</template>
+
+<script>
+import Api from "../../services/index";
+export default {
+    components: {},
+    data() {
+        return {
+			signText:""
+        };
+    },
+	async onLoad() {
+          let res = await this.$api.signText();
+		  if( res.data.data){
+			  this.signText = res.data.data.sign_text;
+		  }
+		  console.log(121321);
+	},
+};
+</script>
+<style lang='scss' scoped>
+</style>
---
+++ src/pages/send/wuliu_text.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="app-container">
+        <rich-text type="text" :nodes="wuliuText"></rich-text>
+    </div>
+</template>
+
+<script>
+import Api from "../../services/index";
+export default {
+    components: {},
+    data() {
+        return {
+			wuliuText:""
+        };
+    },
+	async onLoad() {
+          let res = await this.$api.wuliuText();
+		  if( res.data.data){
+			  this.wuliuText = res.data.data.wuliuText;
+		  } 
+	},
+};
+</script>
+<style lang='scss' scoped>
+</style>
